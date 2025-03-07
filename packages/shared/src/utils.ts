daac0d84506592d181871bdddae6f1bf07b52a6b
xiankq <xiankq@qq.com>
2025-03-07 15:42:16 +0800

    refactor(shared): improve type safety and exclude dist from type checking
    - Rename interface ArrayDifferenceRetrun to ArrayDiffRetrun for consistency
    - Update function name from arrayDifference to arrayDiff for brevity
    - Exclude dist directory

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -1,4 +1,4 @@
-export interface ArrayDifferenceRetrun<T> {
+export interface ArrayDiffRetrun<T> {
   added: T[];
   removed: T[];
 }
@@ -6,10 +6,10 @@ export interface ArrayDifferenceRetrun<T> {
 /**
  * 计算两个数组的差异，返回新增和删除的元素
  */
-export function arrayDifference<T>(
+export function arrayDiff<T>(
   list: T[],
   oldList: T[] | undefined,
-): ArrayDifferenceRetrun<T> {
+): ArrayDiffRetrun<T> {
   const oldListSet = new Set(oldList);
   const added: T[] = list.filter(obj => !oldListSet.has(obj));
   const newListSet = new Set(list);
