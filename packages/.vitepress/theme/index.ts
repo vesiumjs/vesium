379c0899844cfc1ace24fc6f3b574ff3e02f27d9
xiankq <xiankq@qq.com>
2025-05-12 14:25:11 +0800

    style(deps): update styles and CSS framework imports
    - Reorder CSS imports for better organization
    - Replace 'theme.css' with 'theme.scss' for enhanced theming capabilities
    - Ensure consistent usage of CSS frameworks across the project

--- packages/.vitepress/theme/index.ts
+++ packages/.vitepress/theme/index.ts
@@ -3,9 +3,9 @@
 import type { Theme as VitePressTheme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import Layout from './components/layout.vue';
-import 'uno.css';
-import './styles/theme.css';
 import '@unocss/reset/tailwind.css';
+import './styles/theme.scss';
+import 'uno.css';
 
 export default <VitePressTheme> {
   extends: DefaultTheme,
