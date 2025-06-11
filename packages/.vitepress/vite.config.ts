8865243469d7b7e001d3d14ee1b1ec13808b0cb5
xiankq <xiankq@qq.com>
2025-06-11 17:42:57 +0800

    perf(vite.config): conditionally copy Cesium static files
    - Update vite.config.ts to copy Cesium static files only in development mode
    - This change reduces build time and improves performance when building in production

--- packages/.vitepress/vite.config.ts
+++ packages/.vitepress/vite.config.ts
@@ -5,24 +5,28 @@ import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 import VueDevTools from 'vite-plugin-vue-devtools';
 
-export default defineConfig({
-  plugins: [
-    UnpluginCesium(),
-    VueDevTools(),
-    UnoCSS(),
-    Components({
-      dirs: fileURLToPath(new URL('./theme/components', import.meta.url)),
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: fileURLToPath(new URL('./components.d.ts', import.meta.url)),
-    }),
-  ],
-  server: { port: 9574 },
-  publicDir: '.vitepress/public',
-  build: {
-    rollupOptions: {
-      external: [
-        'cesium',
-      ],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      UnpluginCesium({
+        copyStaticFiles: mode === 'development',
+      }),
+      VueDevTools(),
+      UnoCSS(),
+      Components({
+        dirs: fileURLToPath(new URL('./theme/components', import.meta.url)),
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dts: fileURLToPath(new URL('./components.d.ts', import.meta.url)),
+      }),
+    ],
+    server: { port: 9574 },
+    publicDir: '.vitepress/public',
+    build: {
+      rollupOptions: {
+        external: [
+          'cesium',
+        ],
+      },
     },
-  },
+  };
 });
