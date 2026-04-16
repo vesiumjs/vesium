aceda691c9263a4931b4659ae89b08c74a20892f
xiankq <xiankq@qq.com>
2026-04-17 01:06:13 +0800

    docs(special): 更新包描述和添加注释说明
    更新package.json中的描述以更准确反映包用途
    在index.ts中添加注释说明包的未来规划状态

--- packages/special/index.ts
+++ packages/special/index.ts
@@ -1,1 +1,13 @@
+/**
+ * @vesium/special - Special effects and rendering for Cesium
+ *
+ * This package is reserved for future special effects features including:
+ * - Custom material effects
+ * - Advanced post-processing effects
+ * - Custom primitive rendering
+ *
+ * @status Planned - Not yet implemented
+ * @see https://github.com/vesiumjs/vesium/issues
+ */
+
 export {};
