e089953a6c7c893b69e2e937fcb3d39f89abebc5
xiankq <xiankq@qq.com>
2026-04-17 01:11:18 +0800

    test(useImageryLayer): 添加影像图层功能的单元测试

--- packages/core/useImageryLayer/__tests__/index.test.ts
+++ packages/core/useImageryLayer/__tests__/index.test.ts
@@ -2,8 +2,8 @@ import { mount } from '@vue/test-utils';
 import * as Cesium from 'cesium';
 import { describe, expect, it, vi } from 'vitest';
 import { defineComponent, h, nextTick, ref } from 'vue';
-import { createViewer } from '../createViewer';
-import { useImageryLayer } from '../index';
+import { createViewer } from '../../createViewer';
+import { useImageryLayer } from '../../index';
 
 vi.mock('cesium', async (importOriginal) => {
   const actual = await importOriginal() as any;
